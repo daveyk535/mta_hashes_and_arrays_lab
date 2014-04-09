1b0ed81ea63de01829cf11950c02494fa0b47ca5
daveyk535 <dave@phase2minds.com>
2014-04-09 09:41:14 -0700

    Finished the second part of the problem in the morning...

--- mta.rb
+++ mta.rb
@@ -1,30 +1,3 @@
-require 'pry'
-# The hash table of lines that you must fill in
-# Each key in the hash is a line name and the value is an array of stops.
-
-=begin pseuo-code:
-Ask which stop to get on (n, l, s)
-  train_1
-Ask which stop:
-  stop_1
-
-Ask which train to get off on (n, l, s)
-  train_2
-Ask which stop:
-  stop_2
-
-If train1 == train2
-  total_stops = (Distance between stop_1 and stop_2).abs
-    **try and use index
-
-If train_1 != train_2
-  total_stops = (Distance between train_1/stop_1 and train_1/Union station) + (Distance between train_2/Union station and train_2/stop_2)
-    **try and use & and .first
-
-
-Your total trip lenght is #{total_stops}
-=end
-
 
 mta = {
   "n" => ['ts', '34th', '28th-n', '23rd-n', 'us'],
@@ -46,14 +19,13 @@ puts "What train do you want to get off: n, l, s?"
 puts "Which stop: #{mta[train_2]}"
   stop_2 = gets.chomp
 
-#Confirm the user's input
-puts "Your are boarding at #{train_1}, #{stop_1} and exiting at #{train_2}, #{stop_2}"
-
 #same train:
 if train_1 == train_2
 total_stops = (mta[train_1].index(stop_1) - mta[train_2].index(stop_2)).abs
 puts "Your trip length is #{total_stops}"
+
+#different train
+elsif train_1 != train_2
+total_stops = (mta[train_1].index(stop_1) - mta[train_1].index("us")).abs + (mta[train_2].index(stop_2) - mta[train_2].index("us")).abs
+puts "Your trip length is #{total_stops} stops"
 end
-
-#This is as far I've gotten...I'll finish it on the weekend...
-
